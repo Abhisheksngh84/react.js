7808c98be4c79b984ef4e511de3eab9d21b3e781
Abhisheksngh84 <Abhishek25081997@gmail.com>
2022-04-16 18:47:46 +0530

    Assignment2

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-
+ 
+const employee = {
+  name: 'Tom',
+  location: 'New York',
+  bloodGroup: 'B+',
+  age: 28,
+  profileImg: 'https://www.w3schools.com/howto/img_avatar.png'
+}
 
 function App(props) {
  const {employee}=props;
+
+
+  
     
 return(
   <div className="container">
   {/* Start your React code here */}
+  <div>{employee.name}</div>
 </div>
 )
 }
